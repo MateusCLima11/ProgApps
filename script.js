542861c84567a5a023173065e6430ae7157b5a7d
MateusCLima11 <mateuslimasesi@gmail.com>
2026-02-20 14:15:03 -0300

    Correct typos and implement button click functionality
    Fix typos in getElementById method calls and add event listener for button click.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const botaoAdicionar = document.getElementById('btnAdicionar');
-const campoTexto = document.getElementById('novoAprovado');
-const lista = document.getElementById('listaAprovados');
+const botaoAdicionar = docmuent.getElementBy.Id('btnAdicionar');
+const campoTexto = docmuent.getElementBy.Id('novoAprovado');
+const lista = docmuent.getElementBy.Id('listaAprovados');
+
+botaoAdicionar.addEventListener('click',function() {
+    const nome = campoTexto.value;
+
+    if(nome.trim() !== "") {
+        const novoItem = document.createElement('li');
+        novoItem.textContent=nome; 
+
+        lista.appendChild(novoItem);
+
+        campoTexto.value = "";
+        campoTexto.focus();
+    }else{
+        alert("Por favor, Digite um nome.");
+    }
+    
+})
