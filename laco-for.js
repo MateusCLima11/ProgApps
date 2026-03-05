8083411d5d38862724b8e63cb63249a9c12753d4
MateusCLima11 <aluno.den@senaibahia.com.br>
2026-03-05 15:51:06 -0300

    feat:salvando,operadores,while e for.

---
+++ laco-for.js
@@ -0,0 +1,6 @@
+// Laço de repetição: For
+
+let i;
+for (i = 1 ; i <=5; i+2) {
+    console.log(i)
+}
---
+++ operadores.js
@@ -0,0 +1,8 @@
+// Operadores lógicos.
+
+let idade = 10
+let peso = "10"
+
+console.log("-COMPARAÇÃO")
+console.log("Comparando conteúdo: ")
+console.log(idade == peso)
