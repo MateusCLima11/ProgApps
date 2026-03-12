ec6663046db35dce70e70b78aff9b248e6978382
MateusCLima11 <aluno.den@senaibahia.com.br>
2026-03-12 16:43:14 -0300

    feat:salvando.

--- 16.vetores-com-objetos.js
+++ 16.vetores-com-objetos.js
@@ -7,7 +7,7 @@ const listaDeNumeros = [1, 2, 3, 4, 5]
 // Vetor de objetos com dados de nome e idade
 // Um objeto carrega dados com um classe
 const listaDeUsuarios = [
-    {nome: "Ana", Idade: 25},
+    {nome: "Ana", idade: 25},
     {nome: "Maria", Idade: 35},
     {nome: "Joana", Idade: 45}
 ]
@@ -22,14 +22,28 @@ listaDeUsuarios.forEach( usuario => {
 // Como os antigos.
 console.log("\nExibindo todos os usuários do vetor.")
 for (let i = 0; i < listaDeUsuarios.length; i++) {
-    console.log(`${listaDeUsuarios[i].nome} tem ${listaDeUsuarios[i].Idade} anos.`)
+    console.log(`${listaDeUsuarios[i].nome} tem ${listaDeUsuarios[i].idade} anos.`)
 }
 
 console.log("\nFiltrando maiores de 18 anos.")
-const maioridade = listaDeUsuarios.filter(usuario => usuario.Idade >= 18)
+const maioridade = listaDeUsuarios.filter(usuario => usuario.idade >= 18)
 // Use as "{}" caso precise de mais uma linha.
 // Nesse caso não precisa, por isso não foi utilizado.
 maioridade.forEach ( usuario => 
-    console.log(`${usuario.nome} tem ${usuario.Idade} anos.`)
+    console.log(`${usuario.nome} tem ${usuario.idade} anos.`)
 )
+
+
+console.log("\nNa lista de usuários mostre apenas os nomes.")
+const nomes = listaDeUsuarios.map(u => u.nome)
+nomes.forEach(nome => console.log(`Nome: ${nome}`))
+
+console.log("\nNa lista de usuários encontre um usuário.")
+const usuarioEncontrado = listaDeUsuarios.find(u => u.nome === "Ana")
+console.log(`Nome: ${usuarioEncontrado.nome} \nIdade: ${usuarioEncontrado.idade}`)
+
+console.log("\nNa lista de usuários encontre um usuário com 45 anos.")
+const usuario = listaDeUsuarios.find(u => u.idade === 45)
+console.log(`Nome: ${usuario.nome} \nIdade: ${usuario.idade}`)
+
 
---
+++ 17.atividade.js
@@ -0,0 +1,15 @@
+const listaDeAlunos = [
+    {nome: "Ana", nota: 5.0},
+    {nome: "Bruno", nota: 10.0},
+    {nome: "Carla", nota: 2.00},
+    {nome: "Andrea", nota: 7.0},
+    {nome: "Marta", nota: 6.0}
+]
+
+console.log("Encontre a aluna Marta e mostre o nome e média dela.")
+
+console.log("Mostre a média geral da turma.")
+
+console.log("Mostre o nome e nota dos alunos com média abaixo de 7.0.")
+
+console.log("Mostre apenas o nome dos alunos com nota maior ou igual a 7.0.")
