9c0a29a956a1d15181e218e286b17bb9913f4a5b
MateusCLima11 <aluno.den@senaibahia.com.br>
2026-03-19 16:23:40 -0300

    feat:salvando atividade 2 completa e atividade3 parcialmente.

---
+++ 3-atividade/script.js
@@ -0,0 +1,11 @@
+document.getElementById('btnCalcular').addEventListener('click', function() {
+
+let num1 = Number(document.getElementById('nota1').value);
+let num2 = Number(document.getElementById('nota2').value);
+let num3 = Number(document.getElementById('nota3').value);
+
+let media = (num1 + num2 + num3) / 3
+
+if (media >= 7){}
+
+});
